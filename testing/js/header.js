8265f30eb43790944b3888a11ceb27a7c456c553
delta-12 <40147170+delta-12@users.noreply.github.com>
2019-10-23 12:52:29 -0400

    Update header.js

--- testing/js/header.js
+++ testing/js/header.js
@@ -12,10 +12,10 @@ document.write('\
     <link rel="shortcut icon" href="images/delta.ico" />\
 \
     <!-- Bootstrap Core CSS -->\
-    <link href="css/bootstrap.min.css" rel="stylesheet">\
+    <link href="/testing/css/bootstrap.min.css" rel="stylesheet">\
 \
     <!-- Custom CSS: You can use this stylesheet to override any Bootstrap styles and/or apply your own styles -->\
-    <link href="css/custom.css" rel="stylesheet">\
+    <link href="/testing/css/custom.css" rel="stylesheet">\
 \
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->\
     <!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->\
