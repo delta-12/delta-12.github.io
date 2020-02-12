e32d9bc0dbb79de6f24b78fcac32feaab5f1a3fb
delta-12 <40147170+delta-12@users.noreply.github.com>
2020-02-12 09:45:47 -0500

    Create footer.js

---
+++ testing/js/footer.js
@@ -0,0 +1,28 @@
+document.write('\
+<footer>\
+\
+<div class="small-print">\
+<div class="container">\
+<p><a href="terms_and_conditions.html">Terms &amp; Conditions</a> | <a href="privacy.html">Privacy Policy</a> | <a href="contact.html">Contact</a></p>\
+<p>Copyright &copy; delta-12.github.com 2019 (Hehe, Not Really!)</p>\
+</div>\
+</div>\
+</footer>\
+\
+\
+<!-- jQuery -->\
+<script src="js/jquery-1.11.3.min.js"></script>\
+\
+<!-- Bootstrap Core JavaScript -->\
+<script src="js/bootstrap.min.js"></script>\
+\
+<!-- IE10 viewport bug workaround -->\
+<script src="js/ie10-viewport-bug-workaround.js"></script>\
+\
+<!-- Placeholder Images -->\
+<script src="js/holder.min.js"></script>\
+\
+</body>\
+\
+</html>\
+')
