9350eb0d3af4aa08cb637faecdc56993f20925a3
delta-12 <btesq235@gmail.com>
2020-06-04 14:56:30 -0400

    hello world web app

---
+++ testing/web-apps/hello-world/precache-manifest.26c06d76a2b69289289936f664ece8a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5448efc7031a2ee15c68aebfd5e359c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d8da432f840f5152fcf0",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "./static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8da432f840f5152fcf0",
+    "url": "./static/js/main.95ff182b.chunk.js"
+  },
+  {
+    "revision": "7a833fefead15dc83aa8",
+    "url": "./static/js/runtime-main.d6a7b5d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
