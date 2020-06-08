44254be31122d53d53049f4c82d92f89550bc1d6
delta-12 <btesq235@gmail.com>
2020-06-08 17:46:13 -0400

    testing react tutorial

---
+++ testing/files/react-tutorial/precache-manifest.dff40cba019a629f93733b3bbd4a4ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722b189384d2560bf872a03bdc680e61",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1323e9c44e0180aebceb",
+    "url": "./static/css/main.69aeb0d4.chunk.css"
+  },
+  {
+    "revision": "51ac305b51cbe81e390e",
+    "url": "./static/js/2.2dcd46cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2dcd46cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1323e9c44e0180aebceb",
+    "url": "./static/js/main.81c95427.chunk.js"
+  },
+  {
+    "revision": "e7969d140d3c5c16982f",
+    "url": "./static/js/runtime-main.0c758368.js"
+  }
+]);
