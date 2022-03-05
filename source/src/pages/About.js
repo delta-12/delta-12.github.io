ceee63b4569d859628675b6621caa06da4d150e5
delta-12 <btesq235@gmail.com>
2022-03-04 23:12:18 -0500

    update formatting

--- source/src/pages/About.js
+++ source/src/pages/About.js
@@ -11,7 +11,13 @@ export default class About extends Component {
                     <div className="jumbotron mt-5 pt-5">
                         <h1 className="display-4">About</h1>
                         <hr className="my-4" />
-                        <p>Learn more about me.</p>
+                        <p className="lead">
+                            My name is Benjamin Esquieres, and I am currently a first-year engineering student at the University of Pittsburgh,
+                            Swanson School of Engineering.  I have a passion for software, code, and technology, and I like working on interdisciplinary projects
+                            within engineering.  In the coming weeks I intend to delcare a major in Computer Engineering, and I plan on pursuing an Accelerated Master's
+                            degree in Electrical Engineering as well.  I am looking to pursue a career that combines several of my many interests such as
+                            embedded devices, robotics, process control, automation, UAV/drones, and automotive vehicles.
+                        </p>
                     </div>
                 </div>
             </div>
--- source/src/pages/Home.js
+++ source/src/pages/Home.js
@@ -11,13 +11,6 @@ export default class Home extends Component {
                 <div className="container-lg mt-5 pt-5">
                     <div className="jumbotron mt-5 pt-5">
                         <h1 className="display-4">Hello, world!</h1>
-                        <p className="lead">
-                            My name is Benjamin Esquieres, and I am currently a first-year engineering student at the University of Pittsburgh,
-                            Swanson School of Engineering.  I have a passion for software, code, and technology, and I like working on interdisciplinary projects
-                            within engineering.  In the coming weeks I intend to delcare a major in Computer Engineering, and I plan on pursuing an Accelerated Master's
-                            degree in Electrical Engineering as well.  I am looking to pursue a career that combines several of my many interests such as
-                            embedded devices, robotics, process control, automation, UAV/drones, and automotive vehicles.
-                        </p>
                         <hr className="my-4" />
                         <p><Link to="/about">Learn</Link> more about me and <Link to="/projects">explore</Link> some of my recent projects.</p>
                     </div>
